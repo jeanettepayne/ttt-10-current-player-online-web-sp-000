f0aca409e685f6651cb3e324a8d0c84b31475024
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-02 21:37:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,5 +3,5 @@ board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
 def turn_count(board)
   board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
   board.each do |move|
-    
+    puts "This is move #{"
 end
